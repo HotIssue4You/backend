031fe6eb4b3d85b3421a8765d8a216e225c535fe
xxezin <kcmclub22@gmail.com>
2024-04-16 16:25:00 +0900

    [CHORE] 모델 수정 후 마이그레이션

--- mainpage/models.py
+++ mainpage/models.py
@@ -3,11 +3,4 @@ from django.db import models
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    created_at = models.DateTimeField # (auto_now_add=True)
-    link_url = models.URLField(max_length=200)
-    
-class WordCloud(models.Model):
-    image_url = models.URLField(max_length=200)
-    # 여기도 시간대가 있어야하지 않을까?
     created_at = models.DateTimeField
-    
