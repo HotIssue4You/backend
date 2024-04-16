7669347601c3db221569372dffa6e9877eafeaa4
xxezin <kcmclub22@gmail.com>
2024-04-16 14:37:50 +0900

    [FEAT] 날짜, 시간 선택 기능 추가

--- mainpage/views.py
+++ mainpage/views.py
@@ -4,9 +4,10 @@ from django.utils import timezone
 
 # Create your views here.
 def index(request):
+    
     context = {
-        "now" : timezone.localtime(),
-        "latest" : WordCloud.objects.last(),
+        "now" : timezone.localtime().strftime('%Y-%m-%d'), # 현재 시각
+        "latest" : WordCloud.objects.last(), # 가장 최근 저장된 것
         "times" : [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
     }
     return render(request, "index.html", context)
